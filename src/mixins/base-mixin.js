51b243552e1ebf93f6f8a0b096c82ccf9d771eef
Braydon Kains <kainsbraydon@gmail.com>
2020-08-19 09:17:58 -0400

    Modifying base-mixin to allow for langterms (#32)
    * Modifying base-mixin to allow for langterms
    
    * Might as well add Steph while I'm here!

--- src/mixins/base-mixin.js
+++ src/mixins/base-mixin.js
@@ -2,11 +2,25 @@ import { LocalizeMixin } from '@brightspace-ui/core/mixins/localize-mixin';
 import { RtlMixin } from '@brightspace-ui/core/mixins/rtl-mixin';
 
 export const BaseMixin = superclass => class extends RtlMixin(LocalizeMixin(superclass)) {
+	static deepCopy(original) {
+		if (!original || !(original instanceof Object))
+			return original;
+
+		const copy = {};
+		Object.keys(original).forEach(key =>
+			copy[key] = this.deepCopy(original[key])
+		);
+		return copy;
+	}
+
 	static async getLocalizeResources(langs) {
 		for await (const lang of langs) {
 			let translations;
 			switch (lang) {
 				case 'en':
+					translations = await import('../../locales/en.js');
+					break;
+				default:
 					translations = await import('../../locales/en.js');
 					break;
 			}
@@ -20,5 +34,9 @@ export const BaseMixin = superclass => class extends RtlMixin(LocalizeMixin(supe
 		}
 
 		return null;
+	}
+
+	localize(key, params) {
+		return super.localize(key, params) || `{language term '${key}' not found}`;
 	}
 };
